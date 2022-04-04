361ca5a44bd03ae46ab3530b183309ce625a3eb1
TaraMay21 <tmpuff21@gmail.com>
2022-04-04 13:21:29 -0600

    hello-site

---
+++ helloapp/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from helloapp.views import HelloWorldView, HelloView
+
+urlpatterns = [
+    # helloapp/
+    path('', HelloWorldView.as_view(), name='hello_world'),
+    path('<name>', HelloView.as_view(), name='hello_name'),
+    path('', HelloWorldView.as_view(), name='goodbye'),
+]
---
+++ helloapp/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render
+from django.views import View
+
+# Create your views here.
+
+
+class HelloWorldView(View):
+    def get(self, request):
+        return render(request=request, template_name='hello_world.html')
+
+
+class HelloView(View):
+    def get(self, request, name):
+        context = {'name': name}
+        return render(
+            request=request, template_name='hello_name.html', context=context,
+        )
+
+class HelloView(View):
+    def get(self, request, name='world'):
+        context = {'name': name}
+        return render(
+            request=request, template_name='hello_name.html', context=context,
+        ) 
+
+class GoodbyeView(View):
+    def get(self, request):
+        return render(request=request, template_name='goodbye.html')
+
+
+class GoodbyeView(View):
+    def get(self, request, name='Goodbye'):
+        context = {'name': name}
+        return render(
+            request=request, template_name='goodbye.html', context=context,
+        ) 
+      
